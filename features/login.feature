e181087d6f747b652f23d3de1b0388e60ac3334d
Ayush <ayush.srivastava@unthinkable.co>
2026-02-05 18:29:04 +0530

    Added test cases and test-report/screenshot

--- features/login.feature
+++ features/login.feature
@@ -1,14 +1,26 @@
-Feature: OrangeHRM Login
+Feature: OrangeHRM Login Functionality
   As an admin user, I want to login to the portal
   So that I can access dashboard page.
 
+  #Happy Flow
   Scenario: Verify login with valid admin credentials
     Given the user launches the OrangeHRM portal
-    When the user enters username "<username>" and password "<password>"
+    When the user enters username "Admin" and password "admin123"
     And clicks on the login button
     Then the user should be redirected to the dashboard page
 
+#    Examples:
+#      | username | password |
+#      | Admin    | admin123 |
+
+  #Negative Flow
+  Scenario Outline: Verify login with invalid credentials
+    Given the user launches the OrangeHRM portal
+    When the user enters username "<username>" and password "<password>"
+    And clicks on the login button
+    Then the user should see error message "Invalid credentials"
+
     Examples:
       | username | password |
-      | Admin    | admin123 |
-      |
+      | adminnn  | xxxxxxxx |
+      | aadmin   | admin123 |
--- steps_definations/test_login_steps.py
+++ steps_definations/test_login_steps.py
@@ -12,11 +12,11 @@ def open_url(page):
     login_page = LoginPage(page)
     login_page.navigate()
 
-@when(parsers.parse('the user enters credentials for "{user_type}"'))
-def enter_credentials(page, user_type):
-    credentials = get_data(user_type)
+@when(parsers.parse('the user enters username "{username}" and password "{password}"'))
+def enter_credentials(page, username, password):
+    # credentials = get_data(user_type)
     login_page = LoginPage(page)
-    login_page.login(credentials['username'], credentials['password'])
+    login_page.login(username, password)
 
 @when('clicks on the login button')
 def click_login(page):
@@ -25,3 +25,10 @@ def click_login(page):
 @then('the user should be redirected to the dashboard page')
 def verify_login(page):
     expect(page).to_have_url(re.compile(r".*dashboard.*"))
+
+@then(parsers.parse('the user should see error message "{error_msg}"'))
+def verify_error(page, error_msg):
+    login_page = LoginPage(page)
+    actual_error_msg = login_page.error_message
+    expect(actual_error_msg).to_contain_text(error_msg)
+    # assert actual_error_msg == error_msg
